9c9503cbffde64491b5b1adde346ca3232473c93
Aditya Kumar <adityak007mr@gmail.com>
2024-09-01 15:20:54 +0530

    added static site serving

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.enableCors();
+    
+    console.log(__dirname)
+
+    // serve static frontend
+    app.useStaticAssets(path.join(__dirname, '../../..', 'public'))
+
     app.setGlobalPrefix('api/v1');
+    app.use((req, res, next) => {
+        if (!req.path.startsWith('/api/v1')) {
+            res.sendFile(path.join(__dirname, '../../..', 'public', 'index.html'))
+        } else {
+            next()
+        }
+    })
+
     await app.listen(8181);
 }
 bootstrap();
