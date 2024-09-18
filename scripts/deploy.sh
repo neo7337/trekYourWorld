76278328dd5a85273d1b0926c3137d05455782f3
Aditya Kumar <adityak007mr@gmail.com>
2024-09-18 17:15:08 +0530

    deploy.yml update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,21 +2,3 @@
 
 set -e
 
-# clear any existing docker images
-
-# copy the docker compose file to server
-# copy the nginx-configuration to server
-
-# run docker compose up
-
-# stop any existing docker compose
-docker-compose stop
-
-# remove the existing containers
-docker-compose rm -f
-
-# pull the latest containers
-docker-compose pull
-
-# start the containers
-docker-compose up -d
